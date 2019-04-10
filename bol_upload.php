a6459966deaa26bb704d593a04e9013595dbe7b6
MP Droog <mpdroog@icloud.com>
2019-04-10 22:57:05 +0200

    bol_upload delete old offers

--- bol_upload.php
+++ bol_upload.php
@@ -42,6 +42,21 @@ function bol_http($method, $url, $bearer, $d = []) {
         $headers[] = 'Content-Type: application/vnd.retailer.v3+json';
     }
 
+    $res_headers = [];
+    curl_setopt($session, CURLOPT_HEADERFUNCTION, function($curl, $header) use (&$res_headers) {
+        $len = strlen($header);
+        $header = explode(':', $header, 2);
+        if (count($header) < 2) // ignore invalid headers
+          return $len;
+        $name = strtolower(trim($header[0]));
+        if (!array_key_exists($name, $res_headers))
+          $res_headers[$name] = [trim($header[1])];
+        else
+          $res_headers[$name][] = trim($header[1]);
+        return $len;
+    });
+
+
     curl_setopt($session, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($session, CURLOPT_CUSTOMREQUEST, $method);
     curl_setopt($session, CURLOPT_ENCODING, 'UTF-8');
@@ -50,12 +65,38 @@ function bol_http($method, $url, $bearer, $d = []) {
     $response = curl_exec($session);
     curl_close($session);
     $results = json_decode($response, true);
-    return $results;
+    return [$results, $res_headers];
 }
 
-// https://api.bol.com/retailer/public/redoc/v3#operation/post-offer-export
-// Not interesting? https://developers.bol.com/documentatie/ftp/content-ftps-handleiding/
+$db = new core\Db(sprintf("sqlite:%s/db.sqlite", __DIR__), "", "");
 $token = bol_bearer();
 
-$res = bol_http("POST", "/offers/export", $token, ["format" => "CSV"]);
-var_dump($res);
+// TODO: Send everything that changed locally compared to remote?
+$now = time();
+
+// 0.Del  prods in bol_del where tm_synced is null
+foreach ($db->getAll("select bol_id from bol_del where tm_synced is null") as $prod) {
+    list($res, $head) = bol_http("DELETE", "/offers/".$prod["bol_id"], $token, []);
+    if (! in_array($res["status"], ["PENDING", "SUCCESS"])) {
+        var_dump($res);
+        user_error("DELETE offer err.");
+    }
+    $db->exec("UPDATE `bol_del` SET `tm_synced` = ? WHERE `bol_id` = ?", [$now, $prod["bol_id"]]);
+    echo sprintf("bol_del %s\n", $prod["bol_id"]);
+
+    if (! isset($head["x-ratelimit-remaining"])) {
+        var_dump($head);
+        var_dump($res);
+        user_error("Unexpected response.");
+    }
+    if ($head["x-ratelimit-remaining"][0] === "1") {
+        $retry = intval($head["x-ratelimit-reset"][0]) - time();
+        echo sprintf("[ratelimit] sleep %d sec..", $retry);
+        sleep($retry);
+    }
+}
+
+// 1.Sync prods not in Bol
+// 2.Sync prods newer than lastrun.syncdate
+// 3.Save lastrun.syncdate
+
