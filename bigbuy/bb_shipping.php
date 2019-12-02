cd654fa25923035844b2de7da2223ea23442f963
root <root@fra1.rootdev.nl>
2019-12-02 19:56:40 +0000

    Feature. Save shipping cost for estimating total price

--- bigbuy/bb_shipping.php
+++ bigbuy/bb_shipping.php
@@ -12,7 +12,7 @@ const BIGBUY_KEY = "NjZlNTJjNTliODg2ODk5Y2JmZjk4OGI4M2Q1MTRhNjk1YWNhNzQxYmI1YjJl
 function bb_json($url, array $args) {
     $ch = curl_init(BIGBUY_URL . $url);
     curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($args));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($args));
     curl_setopt($ch, CURLOPT_ENCODING, 'UTF-8');
     curl_setopt($ch, CURLOPT_FAILONERROR, true);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
@@ -27,6 +27,7 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($args));
 
     $res = curl_exec($ch);
     if ($res === false) {
+        var_dump($res);
         user_error(curl_error($ch));
     }
     curl_close($ch);
@@ -42,8 +43,23 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($args));
     }
     return $json;
 }
+function parseDate($term) {
+  list($num, $dur) = explode(" ", $term);
+  if (! in_array($dur, ["days", "h"])) {
+    return false;
+  }
+  if (strpos($num, "-") !== false) {
+    $num = substr($num, strpos($num, "-")+1);
+  }
+  if (! is_numeric($num)) {
+    return false;
+  }
+  $m = ["days" => "days", "h" => "hours"];
+  return "+$num " . $m[$dur];
+}
 
 $db = new core\Db(sprintf("sqlite:%s/db.sqlite", __DIR__), "", "");
+$shipping = $db->getAll("select shipping_service_id, weight_modulo from shipping");
 
 $min = "0";
 $max = "10";
@@ -57,10 +73,14 @@ while (true) {
     if ($n > $max) break;
 }
 
+$already = $db->getAllMap("key", "select (shipping_service_id || '-' || weight_modulo) as key, cost from shipping");
+$db->exec("delete from shipestimate");
+$db->exec("delete from shipping");
+
 foreach ($weights as $weight) {
     echo $weight . "\n";
-    $prod = $db->getRow("select name,weight,sku from prods where weight >= ? and weight <= ?", [$weight, bcadd($weight, $step, 1)]);
-    var_dump($prod);
+    $prod = $db->getRow("select name,weight,sku from prods where weight <= ? ORDER BY weight desc", [$weight]);
+    if(VERBOSE) var_dump($prod);
 
     $s = bb_json("/rest/shipping/orders.json", [
   "order" => [
@@ -74,9 +94,52 @@ foreach ($weights as $weight) {
     ]]
   ]
 ]);
-    var_dump($s);
+
+    foreach ($s["shippingOptions"] as $opt) {
+	$key = sprintf("%s-%s", $opt["shippingService"]["id"], $weight);
+	if (isset($already[$key])) {
+            if ($already[$key]["cost"] !== strval($opt["cost"])) {
+                die(sprintf("ERR: shippingService price diff? expect=%s found=%s\n", $already[$key]["cost"], $opt["cost"]));
+	    }
+	    // Already have price
+	    continue;
+	}
+        $db->insert("shipping", [
+            "shipping_service_id" => $opt["shippingService"]["id"],
+	    "shipping_name" => $opt["shippingService"]["name"],
+            "shipping_method" => $opt["shippingService"]["transportMethod"],
+	    "delay" => parseDate($opt["shippingService"]["delay"]),
+	    "cost" => $opt["cost"],
+	    "weight_modulo" => $weight,
+	    "weight_max" => $opt["weight"]
+        ]);
+	if(VERBOSE) echo sprintf(
+	    "Weight(%s) has maxweight=%s method=%s delay=%s cost=%s\n",
+	    $weight, $opt["weight"], $opt["shippingService"]["name"] . "-" . $opt["shippingService"]["transportMethod"], parseDate($opt["shippingService"]["delay"]), $opt["cost"]
+	);
+    }
+
+    // Post-process, get the 2 cheapest and use the most expensive of those
+    // so we can use 1 value after this for price calculations
+    $prices = $db->getAll("select delay,cost from shipping where shipping_method = 'van' and weight_modulo = ? order by cost asc limit 2", [$weight]);
+    if (count($prices) === 0) die("no van shipping method?");
+    $cost = count($prices) === 2 ? $prices[1] : $prices[0];
+
+    /*
+     CREATE TABLE "shipestimate" (
+  "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+  "weight_modulo" real NOT NULL,
+  "delay" TEXT NOT NULL,
+  "cost" real NOT NULL
+);
+     */
+    $db->insert("shipestimate", [
+	    "weight_modulo" => $weight,
+	    "delay" => $cost["delay"],
+	    "cost" => $cost["cost"]
+    ]);
 
     // 1 call per 3sec
     sleep(3);
-
 }
+
