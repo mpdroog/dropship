805f7e8faa423a10af9ef545edeec37ea216af31
root <root@fra1.rootdev.nl>
2019-12-17 17:14:38 +0000

    Feature. Bigbuy check curl_setopt

--- core/bigbuy.php
+++ core/bigbuy.php
@@ -5,20 +5,75 @@ const BIGBUY_KEY = "NjZlNTJjNTliODg2ODk5Y2JmZjk4OGI4M2Q1MTRhNjk1YWNhNzQxYmI1YjJl
 
 function bb_json($url, array $args) {
     $ch = curl_init(BIGBUY_URL . $url);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($args));
-    curl_setopt($ch, CURLOPT_ENCODING, 'UTF-8');
-    //curl_setopt($ch, CURLOPT_FAILONERROR, true);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-    curl_setopt($ch, CURLOPT_AUTOREFERER, true);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 120);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    $ok = 1;
+    $ok &= curl_setopt($ch, CURLOPT_POST, true);
+    $ok &= curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($args));
+    $ok &= curl_setopt($ch, CURLOPT_ENCODING, 'UTF-8');
+    $ok &= curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    $ok &= curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+    $ok &= curl_setopt($ch, CURLOPT_TIMEOUT, 120);
+    $ok &= curl_setopt($ch, CURLOPT_HTTPHEADER, [
         sprintf('Authorization: Bearer %s', BIGBUY_KEY),
         'Content-Type: application/json',
         "Accept: application/json"
     ]);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    //curl_setopt($ch, CURLOPT_POSTFIELDS, $args);
+    $ok &= curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    if ($ok !== 1) {
+        user_error("curl_setopt failed");
+    }
+
+    $res = curl_exec($ch);
+    if ($res === false) {
+        var_dump($res);
+        user_error(curl_error($ch));
+    }
+    $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    if ($res === false) {
+        user_error('curl_exec fail');
+    }
+
+    $json = json_decode($res, true);
+    if (! is_array($json)) {
+        print_r($res);
+        user_error("bb_json fail");
+    }
+    return $json;
+}
+
+function bb_json_header($url, array $args) {
+    $ch = curl_init(BIGBUY_URL . $url);
+    $ok = 1;
+    $ok &= curl_setopt($ch, CURLOPT_POST, true);
+    $ok &= curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($args));
+    $ok &= curl_setopt($ch, CURLOPT_ENCODING, 'UTF-8');
+    $ok &= curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    $ok &= curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+    $ok &= curl_setopt($ch, CURLOPT_TIMEOUT, 120);
+    $ok &= curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        sprintf('Authorization: Bearer %s', BIGBUY_KEY),
+        'Content-Type: application/json',
+        "Accept: application/json"
+    ]);
+    $ok &= curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    $res_headers = [];
+    $ok &= curl_setopt($ch, CURLOPT_HEADERFUNCTION, function($curl, $header) use (&$res_headers) {
+        $len = strlen($header);
+        $header = explode(':', $header, 2);
+        if (count($header) < 2) // ignore invalid headers
+          return $len;
+        $name = strtolower(trim($header[0]));
+        if (!array_key_exists($name, $res_headers))
+          $res_headers[$name] = [trim($header[1])];
+        else
+          $res_headers[$name][] = trim($header[1]);
+        return $len;
+    });
+    if ($ok !== 1) {
+        user_error("curl_setopt failed");
+    }
 
     $res = curl_exec($ch);
     if ($res === false) {
@@ -36,5 +91,5 @@ function bb_json($url, array $args) {
         print_r($res);
         user_error("bb_json fail");
     }
-    return $json;
+    return [$res_headers, $json];
 }
